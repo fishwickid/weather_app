55f9199927e56907cb37a6e7b0431378213e1e9e
fishwickid <chadfishwick@gmail.com>
2020-06-20 15:08:28 +0800

    new changes to JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//create some global variables
+$(document).ready()
+
 function citySearch() {
+
     event.preventDefault();
+    $(".uvIndex").empty();
+
     var currentSearchCity = ($("#current-search-city").val());
     // query URL and custom API KEY variable for current day weather 
     const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentSearchCity + "&units=metric&appid=82c89536a936fdf2b3461ac6bec2669f";
@@ -8,31 +12,25 @@ function citySearch() {
     $.get({
         url: queryURL,
     }).then(function (response) {
-        console.log(response);
-        const weather = response;
-        console.log(weather.name);
-        console.log(weather.main.temp);
-        console.log(weather.main.humidity);
-        console.log(weather.wind.speed);
-        console.log(weather.coord.lon);
-        console.log(weather.coord.lat);
-        const cityName = weather.name;
-        const cityTemp = weather.main.temp;
-        const cityHumidity = weather.main.humidity;
-        const cityWindSpeed = weather.wind.speed;
+        const cityName = response.name;
+        const cityTemp = response.main.temp;
+        const cityHumidity = response.main.humidity;
+        const cityWindSpeed = response.wind.speed;
+        const cityIcon = response.weather[0].icon;
+        $("#city-date").text(moment().format('MMMM Do YYYY'))
         $("#city-name").text(cityName)
-        $("#city-temp").text("Temperature: " + cityTemp.toFixed(1) + "°C")
+        $("#city-icon").attr("src", "https://openweathermap.org/img/w/" + cityIcon + ".png")
+        $("#city-temp").text("Temperature:" + cityTemp.toFixed(1) + "°C")
         $("#city-humidity").text("Humidity: " + cityHumidity + "%")
         $("#city-wind").text("Wind Speed: " + cityWindSpeed.toFixed(2) + "KPH")
-        var lon = weather.coord.lon;
-        var lat = weather.coord.lat;
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
         var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?appid=82c89536a936fdf2b3461ac6bec2669f&lat=" + lat + "&lon=" + lon + "&cnt=1";
         $.ajax({
             url: uvIndexURL,
             method: "GET"
         }).then(function (response) {
             var uvIndFinal = response.value;
-            console.log(response.value);
             $(".uvIndex").append("UV Index: ");
             var uvBtn = $("<button>").text(uvIndFinal);
             $(".uvIndex").append(uvBtn);
@@ -52,107 +50,103 @@ function citySearch() {
                 // If LON&LAT greater than 11, make Purple
                 uvBtn.attr("class", "uvPurple");
             }
-            var getForecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=82c89536a936fdf2b3461ac6bec2669f";
+
+            var getForecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=82c89536a936fdf2b3461ac6bec2669f";
             $.ajax({
                 url: getForecastURL,
                 method: "GET"
             }).then(function (response) {
-                var cityTemp1 = response.list[1].main.temp
-                var cityTemp2 = response.list[2].main.temp
-                var cityTemp3 = response.list[3].main.temp
-                var cityTemp4 = response.list[4].main.temp
-                var cityTemp5 = response.list[5].main.temp
-                var cityHumidity1 = response.list[1].main.humidity
-                var cityHumidity2 = response.list[2].main.humidity
-                var cityHumidity3 = response.list[3].main.humidity
-                var cityHumidity4 = response.list[4].main.humidity
-                var cityHumidity5 = response.list[5].main.humidity
-                var cityIcon1 = response.list[1].weather[0].icon
-                var cityIcon2 = response.list[2].weather[0].icon
-                var cityIcon3 = response.list[3].weather[0].icon
-                var cityIcon4 = response.list[4].weather[0].icon
-                var cityIcon5 = response.list[5].weather[0].icon
-                var iconurl1 = "http://openweathermap.org/img/w/" + cityIcon1 + ".png";
-                var iconurl2 = "http://openweathermap.org/img/w/" + cityIcon2 + ".png";
-                var iconurl3 = "http://openweathermap.org/img/w/" + cityIcon3 + ".png";
-                var iconurl4 = "http://openweathermap.org/img/w/" + cityIcon4 + ".png";
-                var iconurl5 = "http://openweathermap.org/img/w/" + cityIcon5 + ".png";
-                console.log(response.list[1].main.temp)
-                console.log(response.list[1].main.humidity)
-                console.log(response.list[1].weather[0].icon)
-                console.log(response.list[2].main.temp)
-                console.log(response.list[2].main.humidity)
-                console.log(response.list[2].weather[0].icon)
-                console.log(response.list[3].main.temp)
-                console.log(response.list[3].main.humidity)
-                console.log(response.list[4].main.temp)
-                console.log(response.list[4].main.humidity)
-                console.log(response.list[5].main.temp)
-                console.log(response.list[5].main.humidity)
+
+                var cityTemp1 = response.daily[1].temp.day
+                var cityTemp2 = response.daily[2].temp.day
+                var cityTemp3 = response.daily[3].temp.day
+                var cityTemp4 = response.daily[4].temp.day
+                var cityTemp5 = response.daily[5].temp.day
+                var cityHumidity1 = response.daily[1].humidity
+                var cityHumidity2 = response.daily[2].humidity
+                var cityHumidity3 = response.daily[3].humidity
+                var cityHumidity4 = response.daily[4].humidity
+                var cityHumidity5 = response.daily[5].humidity
+                var cityIcon1 = response.daily[1].weather[0].icon
+                var cityIcon2 = response.daily[2].weather[0].icon
+                var cityIcon3 = response.daily[3].weather[0].icon
+                var cityIcon4 = response.daily[4].weather[0].icon
+                var cityIcon5 = response.daily[5].weather[0].icon
+                $("#city-date1").text(moment().add(1, "d").format('D [/] M [/] YY'))
+                $("#city-icon1").attr("src", "https://openweathermap.org/img/w/" + cityIcon1 + ".png")
                 $("#city-temp1").text("Temperature: " + cityTemp1.toFixed(1) + "°C")
                 $("#city-humidity1").text("Humidity: " + cityHumidity1 + "%")
-                $("#cityIcon1").attr('src', iconurl1)
+                $("#city-date2").text(moment().add(2, "d").format('D [/] M [/] YY'))
+                $("#city-icon2").attr("src", "https://openweathermap.org/img/w/" + cityIcon2 + ".png")
                 $("#city-temp2").text("Temperature: " + cityTemp2.toFixed(1) + "°C")
                 $("#city-humidity2").text("Humidity: " + cityHumidity2 + "%")
-                $("#cityIcon2").attr('src', iconurl2)
+                $("#city-date3").text(moment().add(3, "d").format('D [/] M [/] YY'))
+                $("#city-icon3").attr("src", "https://openweathermap.org/img/w/" + cityIcon3 + ".png")
                 $("#city-temp3").text("Temperature: " + cityTemp3.toFixed(1) + "°C")
                 $("#city-humidity3").text("Humidity: " + cityHumidity3 + "%")
-                $("#cityIcon3").attr('src', iconurl3)
+                $("#city-date4").text(moment().add(4, "d").format('D [/] M [/] YY'))
+                $("#city-icon4").attr("src", "https://openweathermap.org/img/w/" + cityIcon4 + ".png")
                 $("#city-temp4").text("Temperature: " + cityTemp4.toFixed(1) + "°C")
                 $("#city-humidity4").text("Humidity: " + cityHumidity4 + "%")
-                $("#cityIcon4").attr('src', iconurl4)
+                $("#city-date5").text(moment().add(5, "d").format('D [/] M [/] YY'))
+                $("#city-icon5").attr("src", "https://openweathermap.org/img/w/" + cityIcon5 + ".png")
                 $("#city-temp5").text("Temperature: " + cityTemp5.toFixed(1) + "°C")
                 $("#city-humidity5").text("Humidity: " + cityHumidity5 + "%")
-                $("#cityIcon5").attr('src', iconurl5)
-            })//Keep city searached by the guests
-            var keepCities = [];
-            var displayCity = $("#recent-searches");
-            var searchButton = $("#btnSearch");
-            var cityInput = $("#city-name");
-             // Function for displaying city names 
-             function renderCityNames() {
-               displayCity.innerHTML = "";
-               
-            $("li").empty()
-               // Render a new city for each search
-               for (var i = 0; i < keepCities.length; i++) {
-              var keepCity = keepCities[i];
-              var li = $("<li>");
-              li.text(keepCities[i]);
-              li.attr("data-index", i);
-              var button = $("<p>");
-              //button.text("City Searched");
-              li.append(button);
-              displayCity.append(li);
-              
-               }
-             }
-            function init () {
-            //stored city names from local storage
-            var storedCityNames = JSON.parse(localStorage.getItem("KeepCities"));
-            //update local storage if keepCities 
-            if (storedCityNames !==null){
-               keepCities = storedCityNames;
-            }
-            renderCityNames();
-            }
-            function storeCityNames (){
-            localStorage.setItem("keepCities", JSON.stringify(keepCities));
-            }
-            searchButton.on("click", function (event){
-            event.preventDefault();
-            var cityTextDisplay = cityInput.val();
-            if (cityTextDisplay ==="") {
-               return;
-            }
-            keepCities.push(cityTextDisplay);
-            cityInput.value = "";
-            storeCityNames();
-            renderCityNames();
             })
-            
         })
     });
 }
 $("#btnSearch").on("click", citySearch)
-
+let city = "";
+let search_history = JSON.parse(localStorage.getItem("cities")) === null ? [] : JSON.parse(localStorage.getItem("cities"));
+//Keep city searached by the guests
+var keepCities = [];
+var displayCity = $("#recent-searches");
+var searchButton = $("#btnSearch");
+var cityInput = $("#current-search-city");
+// Function for displaying city names 
+function renderCityNames() {
+    displayCity.innerHTML = "";
+    $("li").empty()
+    // Render a new city for each search
+    for (var i = 0; i < keepCities.length; i++) {
+        var keepCity = keepCities[i];
+        var li = $("<li>");
+        li.text(keepCities[i]);
+        li.attr("data-index", i);
+        var button = $("<p>");
+        //button.text("City Searched");
+        li.append(button);
+        displayCity.append(li);
+    }
+}
+function init() {
+    //stored city names from local storage
+    var storedCityNames = JSON.parse(localStorage.getItem("KeepCities"));
+    //update local storage if keepCities 
+    if (storedCityNames !== null) {
+        keepCities = storedCityNames;
+    }
+    renderCityNames();
+}
+function storeCityNames() {
+    localStorage.setItem("keepCities", JSON.stringify(keepCities));
+}
+searchButton.on("click", function (event) {
+    event.preventDefault();
+    var cityTextDisplay = cityInput.val();
+    if (cityTextDisplay === "") {
+        return;
+    }
+    keepCities.push(cityTextDisplay);
+    cityInput.value = "";
+    storeCityNames();
+    renderCityNames();
+})
+function clearHistory() {
+    console.log("clicked")
+    $("#recent-searches").empty();
+    keepCities = [];
+    localStorage.setItem("cities", JSON.stringify(keepCities));
+}
+$("#clear-all").on("click", clearHistory);
